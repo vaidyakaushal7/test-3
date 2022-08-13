8e6ca29e7ccee54b8e61118be02d0545ecf65751
kaushal <vaidyakaushal7@gmail.com>
2022-08-13 14:43:16 +0530

    thirdcommit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,9 @@
+﻿export class User {
+    id: number;
+    username: string;
+    tenantname: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token?: string;
+}
---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,41 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('Administrator')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string,tenantid: string, password: string) {
+
+        const header=new HttpHeaders({'tenantId': tenantid, "Access-Control-Allow-Origin": "*"})
+
+        return this.http.post<any>(`http://18.163.187.49:8443/cpq/authenticate`, { username, password }, {headers:header})
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('Administrator', JSON.stringify(user));
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('Administrator');
+        this.currentUserSubject.next(null);
+    }
+}
